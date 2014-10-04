313bb5e0cc01952e8ee5df63716bb326e867281c
Drew Blaisdell <drew.blaisdell@gmail.com>
2014-10-04 01:48:19 -0700

    Added comments, _prefixed private methods.

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var monitor = require('./socket.io-monitor')(io);
+var monitor = require('./socket.io-monitor');
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+monitor(io, {
+  testLatency: true
 });
 
-app.use(express.static(__dirname + '/public'));
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+app.use(express.static(__dirname));
 
 io.on('connection', function(socket) {
   // console.log(socket);
--- socket.io-monitor.js
+++ socket.io-monitor.js
@@ -1,33 +1,52 @@
+// Module dependencies.
+ 
 var ansi = require('ansi');
 
-var Monitor = function(io) {
+// Module exports.
+
+module.exports = Monitor;
+
+// Monitor constructor.
+// Accepts an instance of Socket.IO
+
+function Monitor(io, options) {
   if (!(this instanceof Monitor)) {
-    return new Monitor(io);
+    return new Monitor(io, options);
   }
+
+  options = options || {};
+  this.testLatency = options.testLatency || false;
+  this.timeBetweenEchoes = options.timeBetweenEchoes || 500;
 
   this.ansi = ansi;
   this.cursor = ansi(process.stdout);
   this.sockets = {};
 
+  // Tell Socket.IO to use Monitor.IO.
   io.use(this.middleware.bind(this));
 
-  this.clearPush();
+  // Log new lines to make room for the application.
+  this._addNewLines();
 
-  setInterval(this.tickAndRender.bind(this), 1000);
+  // Hide the cursor.
+  this.cursor.hide();
+
+  // Show the cursor on exit.
+  var self = this;
+  var exitHandler = function() {
+    self.cursor.show();
+    process.exit();
+  };
+
+  process.on('exit', exitHandler);
+  process.on('SIGINT', exitHandler);
+
+  // Run the application.
+  setInterval(this._tick.bind(this), 1000);
 };
 
-Monitor.prototype.clear = function() {
-  var windowHeight = process.stdout.getWindowSize()[1];
-
-  for (var i = 1; i <= windowHeight; i++) {
-    this.cursor
-      .goto(1, i)
-      .eraseLine()
-      .write('');
-  }
-};
-
-Monitor.prototype.clearPush = function() {
+// Log as many new lines as the height of the window.
+Monitor.prototype._addNewLines = function() {
   this.cursor
     .write(Array.apply(null, Array(process.stdout.getWindowSize()[1])).map(function() {
       return '\n';
@@ -36,6 +55,22 @@ Monitor.prototype.clearPush = function() {
     .goto(1, 1);
 };
 
+// Clear the console, starting with the given line.
+Monitor.prototype._clear = function(line) {
+  var windowHeight = process.stdout.getWindowSize()[1];
+
+  for (var i = line; i <= windowHeight; i++) {
+    this.cursor.goto(1, i).eraseLine().write('');
+  }
+};
+
+// Emits a timestamp to the given socket in order to measure latency.
+Monitor.prototype._echo = function(socket) {
+  socket.emit('_echo', Date.now());
+};
+
+// Middleware function for Socket.IO. It is passed each socket when
+// it connects and saves a reference.
 Monitor.prototype.middleware = function(socket, next) {
   var self = this;
 
@@ -43,25 +78,29 @@ Monitor.prototype.middleware = function(socket, next) {
     socket: socket,
     latency: undefined
   };
-  socket.on('_echo', this.receiveEcho.bind(this, socket));
 
-  setInterval(function() {
-    self.echo(socket);
-  }, 2000);
+  if (this.testLatency) {
+    socket.on('_echo', this._receiveEcho.bind(this, socket));
+    this._echo(socket);
+  }
 
   next();
 };
 
-Monitor.prototype.echo = function(socket) {
-  socket.emit('_echo', Date.now());
-};
-
-Monitor.prototype.receiveEcho = function(socket, message) {
+// Handles a return echo from a socket and issues a new echo.
+Monitor.prototype._receiveEcho = function(socket, message) {
   var latency = Date.now() - message;
   this.sockets[socket.id].latency = latency;
+
+  var self = this;
+  setTimeout(function() {
+    self._echo(socket);
+  }, this.timeBetweenEchoes);
 };
 
-Monitor.prototype.removeDisconnectedSockets = function() {
+// Removes sockets that are flagged as disconnected from internal
+// list of sockets. 
+Monitor.prototype._removeDisconnectedSockets = function() {
   var socketIDs = Object.keys(this.sockets),
     current;
 
@@ -74,22 +113,26 @@ Monitor.prototype.removeDisconnectedSockets = function() {
   }
 };
 
-Monitor.prototype.render = function() {
+// Renders the current state of the application in the terminal.
+Monitor.prototype._render = function() {
   var socketIDs = Object.keys(this.sockets);
 
-  this.clear();
-  this.resetCursor();
+  this._clear(1);
+  this._resetCursor();
 
   if (socketIDs.length === 0) {
-    console.log('No sockets connected.');
+    this.cursor.write('No sockets connected.');
   } else {
     for (var i = 0; i < socketIDs.length; i++) {
-      this.renderSocket(socketIDs[i]);
+      this._renderSocket(socketIDs[i]);
     }
   }
+
+  this.cursor.hide();
 };
 
-Monitor.prototype.renderSocket = function(socketID) {
+// Renders a single socket in the terminal.
+Monitor.prototype._renderSocket = function(socketID) {
   current = this.sockets[socketID];
 
   if (current.socket.disconnected) {
@@ -114,22 +157,14 @@ Monitor.prototype.renderSocket = function(socketID) {
   this.cursor.write('\n');
 };
 
-Monitor.prototype.resetCursor = function() {
-  this.cursor
-    .horizontalAbsolute(0)
-    // .eraseLine()
-    .goto(1, 1)
-    .eraseLine()
-    .write('');
+// Moves the cursor to the top left of the window.
+Monitor.prototype._resetCursor = function() {
+  this.cursor.horizontalAbsolute(0).goto(1, 1).eraseLine().write('');
 };
 
-Monitor.prototype.tick = function() {
-  this.removeDisconnectedSockets();
+// Updates internal data, renders the application.
+Monitor.prototype._tick = function() {
+  this._removeDisconnectedSockets();
+  this._render();
 };
 
-Monitor.prototype.tickAndRender = function() {
-  this.tick();
-  this.render();
-};
-
-module.exports = Monitor;
